c76472131f502c7cda297bc93403066b93d94662
金刚 <1219318552@qq.com>
2024-05-19 08:02:01 +0800

    尝试修复部分地区svg图标不显示的问题

--- TuneLab/GUI/SvgIcon.cs
+++ TuneLab/GUI/SvgIcon.cs
@@ -2,6 +2,7 @@
 using Avalonia.Svg.Skia;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ internal class SvgIcon(string content)
 {
     public SvgImage GetImage(Color color)
     {
-        return new SvgImage() { Source = SvgSource.LoadFromSvg(content.Replace("white", string.Format(rgba, color.R, color.G, color.B, color.A / 255.0))) };
+        return new SvgImage() { Source = SvgSource.LoadFromSvg(content.Replace("white", string.Format(rgba, color.R, color.G, color.B, (color.A / 255.0).ToString(new CultureInfo("zh-CN"))))) };
     }
 
     static readonly string rgba = "rgba({0},{1},{2},{3})";
