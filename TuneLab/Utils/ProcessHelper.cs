bcb744f66ce30a4d4b181a87156c96f4eec68fb2
金刚 <1219318552@qq.com>
2024-06-10 00:37:01 +0800

    创建进程改为使用系统库

--- TuneLab/Utils/ProcessHelper.cs
+++ TuneLab/Utils/ProcessHelper.cs
@@ -1,6 +1,6 @@
-﻿using Avalonia.Controls.Shapes;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -10,73 +10,25 @@ namespace TuneLab.Utils;
 
 internal static class ProcessHelper
 {
-    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-    static extern bool CreateProcess(
-        string? lpApplicationName,
-        string lpCommandLine,
-        IntPtr lpProcessAttributes,
-        IntPtr lpThreadAttributes,
-        bool bInheritHandles,
-        uint dwCreationFlags,
-        IntPtr lpEnvironment,
-        string? lpCurrentDirectory,
-        [In] ref STARTUPINFO lpStartupInfo,
-        out PROCESS_INFORMATION lpProcessInformation);
-
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-    public struct STARTUPINFO
+    public static Process CreateProcess(string applicationPath, IEnumerable<string> args)
     {
-        public int cb;
-        public string lpReserved;
-        public string lpDesktop;
-        public string lpTitle;
-        public int dwX;
-        public int dwY;
-        public int dwXSize;
-        public int dwYSize;
-        public int dwXCountChars;
-        public int dwYCountChars;
-        public int dwFillAttribute;
-        public int dwFlags;
-        public short wShowWindow;
-        public short cbReserved2;
-        public IntPtr lpReserved2;
-        public IntPtr hStdInput;
-        public IntPtr hStdOutput;
-        public IntPtr hStdError;
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct PROCESS_INFORMATION
-    {
-        public IntPtr hProcess;
-        public IntPtr hThread;
-        public int dwProcessId;
-        public int dwThreadId;
-    }
-
-    public static PROCESS_INFORMATION CreateProcess(string applicationPath, IEnumerable<string> args)
-    {
-        STARTUPINFO startupInfo = new();
-
-        string commandLine = "\"" + applicationPath + "\" ";
+        var commandLine = string.Empty;
         foreach (var arg in args)
         {
             commandLine += "\"" + arg + "\" ";
         }
 
-        CreateProcess(
-            null,
-            commandLine,
-            IntPtr.Zero,
-            IntPtr.Zero,
-            false,
-            0,
-            IntPtr.Zero,
-            null,
-            ref startupInfo,
-            out var processInfo);
+        Process process = new Process()
+        {
+            StartInfo = new ProcessStartInfo()
+            {
+                UseShellExecute = true,
+                FileName = applicationPath,
+                Arguments = commandLine,
+            }
+        };
+        process.Start();
 
-        return processInfo;
+        return process;
     }
 }
