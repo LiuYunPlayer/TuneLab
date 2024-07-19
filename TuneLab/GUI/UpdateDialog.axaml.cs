92dddc6b28718a031305d6ce7ad46f378750e08f
Kurokitu <foxiti.a@gmail.com>
2024-07-20 02:44:23 +0800

    UpdateLog更换为Markdown并封装了新的弹窗组件

---
+++ TuneLab/GUI/UpdateDialog.axaml.cs
@@ -0,0 +1,78 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using System;
+using TuneLab.Utils;
+using Button = TuneLab.GUI.Components.Button;
+using Markdown.Avalonia;
+using TuneLab.I18N;
+
+namespace TuneLab.GUI;
+
+internal partial class UpdateDialog : Window
+{
+    public enum ButtonType
+    {
+        Primary,
+        Normal
+    }
+
+    private Grid titleBar;
+    private Label titleLabel;
+    private SelectableTextBlock messageTextBlock;
+    private MarkdownScrollViewer markDownScrollViewer;
+
+    public UpdateDialog()
+    {
+        InitializeComponent();
+        Focusable = true;
+        CanResize = false;
+        WindowState = WindowState.Normal;
+        WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        Topmost = true;
+
+        this.DataContext = this;
+        this.Background = Style.BACK.ToBrush();
+        Content.Background = Style.INTERFACE.ToBrush();
+
+        titleBar = this.FindControl<Grid>("TitleBar") ?? throw new InvalidOperationException("TitleBar not found");
+        titleLabel = this.FindControl<Label>("TitleLabel") ?? throw new InvalidOperationException("TitleLabel not found");
+        messageTextBlock = this.FindControl<SelectableTextBlock>("MessageTextBlock") ?? throw new InvalidOperationException("MessageTextBlock not found");
+        markDownScrollViewer = this.FindControl<MarkdownScrollViewer>("MarkDownScrollViewer") ?? throw new InvalidOperationException("MarkDownScrollViewer not found");
+
+        titleLabel.Content = "Update Available".Tr(TC.Dialog);
+    }
+
+    public void SetMessage(string message)
+    {
+        messageTextBlock.Text = message;
+    }
+
+    public void SetMDMessage(string message)
+    {
+        markDownScrollViewer.Markdown = message;
+    }
+
+    public Button AddButton(string text, ButtonType type)
+    {
+        ButtonsPanel.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+        var button = new Button() { MinWidth = 96, Height = 40 };
+
+        if (type == ButtonType.Primary)
+            button.AddContent(new() { Item = new BorderItem() { CornerRadius = 6 }, ColorSet = new() { Color = Style.BUTTON_PRIMARY, HoveredColor = Style.BUTTON_PRIMARY_HOVER } });
+
+        if (type == ButtonType.Normal)
+            button.AddContent(new() { Item = new BorderItem() { CornerRadius = 6 }, ColorSet = new() { Color = Style.BUTTON_NORMAL, HoveredColor = Style.BUTTON_NORMAL_HOVER } });
+
+        button.AddContent(new() { Item = new TextItem() { Text = text }, ColorSet = new() { Color = type == ButtonType.Primary ? Colors.White : Style.LIGHT_WHITE } });
+
+        button.Clicked += Close;
+        var buttonStack = new StackPanel() { Orientation = Avalonia.Layout.Orientation.Horizontal, HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center, Children = { button }, Height = 40, Margin = new(0) };
+
+        Grid.SetColumn(buttonStack, ButtonsPanel.ColumnDefinitions.Count - 1);
+        ButtonsPanel.Children.Add(buttonStack);
+
+        return button;
+    }
+}
+
