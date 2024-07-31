feb8ba75fb4944d8dd279e92472ffd7abec0b9b0
WuChang <3142324836@qq.com>
2024-07-31 13:43:22 +0800

    Fixed CI script path

--- CIUtils/find-executable.sh
+++ CIUtils/find-executable.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Get the directory of the current script
+script_dir=$(dirname "$0")
+
 # Detect the operating system
 os_type=$(uname)
 
 # Execute the corresponding script based on the operating system
 case "$os_type" in
   Linux)
-    echo $(./find-elf.sh "$1")
+    echo $("$script_dir/find-elf.sh" "$1")
     ;;
   Darwin)
-    echo $(./find-macho.sh "$1")
+    echo $("$script_dir/find-macho.sh" "$1")
     ;;
   *)
     echo "Unsupported OS: $os_type"
