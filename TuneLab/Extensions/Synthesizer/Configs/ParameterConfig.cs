6998ea92461099bd054307f24010cd722809e3b0
金刚 <1219318552@qq.com>
2024-09-15 02:05:51 +0800

    feat: parameter config

---
+++ TuneLab/Extensions/Synthesizer/Configs/ParameterConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuneLab.Extensions.Synthesizer.Configs;
+
+internal class ParameterConfig
+{
+    public string id = string.Empty;
+    public string type = string.Empty;
+    public double min_value = double.NegativeInfinity;
+    public double max_value = double.PositiveInfinity;
+    public object? default_value = null;
+    public string[]? options = null;
+}
