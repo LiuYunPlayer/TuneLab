9013f21d06162679d5c8bd66dc3dc674a0de5b64
mhbalthasar <mhbalthasar@cmcc.cc>
2024-11-12 21:35:57 +0800

    MenuBar

--- TuneLab/UI/MainWindow/Editor/Editor.cs
+++ TuneLab/UI/MainWindow/Editor/Editor.cs
@@ -825,7 +825,7 @@ internal class Editor : DockPanel, PianoWindow.IDependency, TrackWindow.IDepende
                     var fileList = files.Select(f => f.TryGetLocalPath()).Where(f => f != null).ToArray();
                     if (fileList != null) InstallExtensions(fileList);
                 });
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) menuBarItem.Items.Add(menuItem);
+                menuBarItem.Items.Add(menuItem);
             }
             menu.Items.Add(menuBarItem);
         }
@@ -857,6 +857,29 @@ internal class Editor : DockPanel, PianoWindow.IDependency, TrackWindow.IDepende
                 void UpdateHeader() => menuItem.SetName(AudioEngine.IsPlaying ? "Pause".Tr(TC.Menu) : "Play".Tr(TC.Menu));
                 AudioEngine.PlayStateChanged += UpdateHeader;
                 TranslationManager.CurrentLanguage.Modified.Subscribe(UpdateHeader);
+                menuBarItem.Items.Add(menuItem);
+            }
+            menu.Items.Add(menuBarItem);
+        }
+
+        {
+            var menuBarItem = new MenuItem { Foreground = Style.TEXT_LIGHT.ToBrush(), Focusable = false }.SetTrName("Extensions");
+            {
+                var menuItem = new MenuItem().SetTrName("Install/Update").SetAction(async () =>
+                {
+                    var topLevel = TopLevel.GetTopLevel(this);
+                    if (topLevel == null)
+                        return;
+                    var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+                    {
+                        Title = "Open Tlx File",
+                        AllowMultiple = false,
+                        FileTypeFilter = [new("Tlx Package") { Patterns = ["*.tlx"] }]
+                    });
+                    if (files.IsEmpty()) return;
+                    var fileList = files.Select(f => f.TryGetLocalPath()).Where(f => f != null).ToArray();
+                    if (fileList != null) InstallExtensions(fileList);
+                });
                 menuBarItem.Items.Add(menuItem);
             }
             menu.Items.Add(menuBarItem);
