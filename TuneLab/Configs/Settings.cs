c43ce077ff72adeaad4242f9950c78cbb2b70488
金刚 <1219318552@qq.com>
2025-02-24 12:31:38 +0800

    fix: 修复背景图不透明度问题未持久化的问题

--- TuneLab/Configs/Settings.cs
+++ TuneLab/Configs/Settings.cs
@@ -47,6 +47,7 @@ internal static class Settings
         Language.Value = settingsFile.Language;
         MasterGain.Value = settingsFile.MasterGain;
         BackgroundImagePath.Value = settingsFile.BackgroundImagePath;
+        BackgroundImageOpacity.Value = settingsFile.BackgroundImageOpacity;
         ParameterBoundaryExtension.Value = settingsFile.ParameterBoundaryExtension;
         PianoKeySamplesPath.Value = settingsFile.PianoKeySamplesPath;
         AutoSaveInterval.Value = settingsFile.AutoSaveInterval;
@@ -66,6 +67,7 @@ internal static class Settings
                 Language = Language,
                 MasterGain = MasterGain,
                 BackgroundImagePath = BackgroundImagePath,
+                BackgroundImageOpacity = BackgroundImageOpacity,
                 ParameterBoundaryExtension = ParameterBoundaryExtension,
                 PianoKeySamplesPath = PianoKeySamplesPath,
                 AutoSaveInterval = AutoSaveInterval,
