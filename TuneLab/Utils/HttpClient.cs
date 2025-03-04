3556ee2df68dfe34673e9c6242c65ce1e8a020f3
金刚 <1219318552@qq.com>
2025-03-04 22:47:50 +0800

    chore: 修正命名空间

--- TuneLab/Utils/HttpClient.cs
+++ TuneLab/Utils/HttpClient.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using TuneLab.Foundation.Utils;
 
-namespace TuneLab.Base.Utils;
+namespace TuneLab.Utils;
 
 public class ApiResponse<T>
 {
